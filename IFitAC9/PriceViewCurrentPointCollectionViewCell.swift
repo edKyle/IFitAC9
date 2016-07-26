b79bf581505127acbe7c850c0f0d5f025d5fac04
TimothyYeou <timyeou1234@hotmail.com>
2016-07-26 16:23:14 +0800

    Add new View

---
+++ IFitAC9/PriceViewCurrentPointCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  PriceViewCurrentPointCollectionViewCell.swift
+//  IFitAC9
+//
+//  Created by YeouTimothy on 2016/7/26.
+//  Copyright © 2016年 Alphacamp. All rights reserved.
+//
+
+import UIKit
+
+class PriceViewCurrentPointCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var itemWidthConstraint: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+    }
+
+}
